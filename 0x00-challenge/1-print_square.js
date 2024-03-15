5dbd8c8935d1cd2dff536662748e62c5819be5f2
Mohammed <mrcloud.med@gmail.com>
2024-03-15 12:10:39 +0100

    Update

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -4,6 +4,8 @@
     
     The size of the square must be the first argument 
     of the program.
+
+    change in size = parseInt(process.argv[2], 16)
 */
 
 
@@ -15,8 +17,9 @@ if (process.argv.length <= 2) {
 }
 
 size = parseInt(process.argv[2], 10)
+
 for (let i = 0 ; i < size ; i ++) {
-        for (let j = 0 ; j < size ; j ++) {
+    for (let j = 0 ; j < size ; j ++) {
         process.stdout.write("#");
     }
     process.stdout.write("\n");
